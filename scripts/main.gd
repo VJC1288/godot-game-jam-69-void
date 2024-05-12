d2ff6fbb028f4feb942cee415861d520ad53742b
Vincent <vjc1988@gmail.com>
2024-05-12 10:38:55 -0400

    Refactored Player Node to not include camera

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,18 +9,17 @@ const PAUSE_MENU = preload("res://scenes/pause_menu.tscn")
 @onready var enemy_attacks_container = $OrbiterManager/EnemyAttacksContainer
 
 var paused = null
-var playerSpawnLocation = Vector3(0,0,-1000)
+
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	spawn_player(playerSpawnLocation)
+	spawn_player()
 
-func spawn_player(location:Vector3):
+func spawn_player():
 	var player = PLAYER.instantiate()
 	player.fireLaser.connect(orbiter_manager.spawn_player_laser)
-	orbiter_manager.player_container.add_child(player)
+	orbiter_manager.camera_pivot.add_child(player)
 	orbiter_manager.current_player = player
-	player.global_position = location
 	Globals.current_player = player
 
 func _input(event):
