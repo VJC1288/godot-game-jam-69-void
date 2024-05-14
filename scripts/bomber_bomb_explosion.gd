858f21adf9e240e3add077349c19c5f7778a8c78
Vincent <vjc1988@gmail.com>
2024-05-14 18:45:04 -0400

    Updated bomber explosion and adjusted wall bounce.

--- scripts/bomber_bomb_explosion.gd
+++ scripts/bomber_bomb_explosion.gd
@@ -5,12 +5,20 @@ extends Area3D
 @export var damage: int
 
 var isLaser:bool = false
+var explosion_time: = 3.0
+var fade_time: = 0.1
 
 func _ready():
+	
 	var scale_tween = create_tween()
-	scale_tween.tween_property(self, "scale", Vector3(6,6,6), .1)
+	scale_tween.tween_property(self, "scale", Vector3(15,15,15), explosion_time)
 	
 	var alpha_tween = create_tween()
-	alpha_tween.tween_property(mesh_instance_3d.mesh.material, "albedo_color", Color(1, 0.271, 0.176, .1), .1)
-	await get_tree().create_timer(.1, false).timeout
+	alpha_tween.tween_property(mesh_instance_3d.mesh.material, "albedo_color", Color(1, 0.271, 0.176, .5), explosion_time)
+	alpha_tween.tween_property(mesh_instance_3d.mesh.material, "albedo_color", Color(1, 0.271, 0.176, 0), fade_time)
+	
+
+	get_tree().create_timer(explosion_time+fade_time, false).timeout.connect(destroy_pickup)
+	
+func destroy_pickup():
 	queue_free()
