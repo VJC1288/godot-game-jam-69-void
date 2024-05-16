06ce4a6454d61dd74c6e22d56cac9d4a1d65b9ec
Vincent <vjc1988@gmail.com>
2024-05-16 18:37:41 -0400

    Added Easter egg sounds for clicking "Drive" on main menu

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -6,6 +6,9 @@ const HOW_TO_PLAY = preload("res://scenes/how_to_play.tscn")
 @onready var play_button = %PlayButton
 @onready var how_to_play = %HowToPlay
 @onready var good_luck = $GoodLuck
+@onready var easter_egg_sounds = $EasterEggSounds
+
+var easter_times_clicked: int = 0
 
 func _ready():
 	play_button.grab_focus()
@@ -27,3 +30,10 @@ func _on_play_button_mouse_entered():
 func _on_how_to_play_mouse_entered():
 	how_to_play.grab_focus()
 	GlobalAudioManager.menu_move_sound.play()
+
+
+func _on_easter_button_pressed():
+	easter_times_clicked += 1
+	if easter_times_clicked % 5 == 0:
+		if easter_egg_sounds.get_child_count() != 0:
+			easter_egg_sounds.get_children().pick_random().play()
